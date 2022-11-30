91c67bdf9c175f4be6873ed80bc234a5ae6d299c
Deal W Sanabria <martinoli944@gmail.com>
2022-11-30 13:20:27 -0600

    initial commit -m

---
+++ main.cpp
@@ -0,0 +1,202 @@
+#include<stdio.h>
+#include<locale.h>
+#include<string.h>
+
+int main() {
+    setlocale(LC_ALL, "");
+    int recu;
+    recu = 1;
+    do {
+        int transistor, piezas, total, producto, mes, prod, posicion1, posicion2, val1, val2;
+        float sumprod = 0, prom;
+        int  encapsulado, cantrans, tipotrans, espt, esptcolum, continente, pc,mcanttransis[5][5]={
+                {20,20,20,20,20},
+                {30,30,30,30,30,},
+                {40,40,40,40,40,},
+                {50,50,50,50,50,},
+                {20,20,20,20,20},
+        };
+        float mespecificaciones[25][5] ={
+                {100,20,3,3, 'NPN'},
+                {100,20,3,3, 'NPN'},
+                {100,20,3,3, 'NPN'},
+                {100,20,3,3, 'NPN'},
+                {100,20,3,3, 'NPN'},
+                {100,20,3,3, 'NPN'},
+                {100,20,3,3, 'NPN'},
+                {100,20,3,3, 'NPN'},
+                {100,20,3,3, 'NPN'},
+                {100,20,3,3, 'NPN'},
+                {100,20,3,3, 'NPN'},
+                {100,20,3,3, 'NPN'},
+                {100,20,3,3, 'NPN'},
+                {100,20,3,3, 'NPN'},
+                {100,20,3,3, 'NPN'},
+                {100,20,3,3, 'NPN'},
+                {100,20,3,3, 'NPN'},
+                {100,20,3,3, 'NPN'},
+                {100,20,3,3, 'NPN'},
+                {100,20,3,3, 'NPN'},
+                {100,20,3,3, 'NPN'},
+                {100,20,3,3, 'NPN'},
+                {100,20,3,3, 'NPN'},
+                {100,20,3,3, 'NPN'},
+                {100,20,3,3, 'NPN'},
+        },
+        //cost de envio
+        vcostenv[5] = {100.50, 200.50, 300.50, 400.50, 500.50};
+        char op, buskencapsulado[15],
+        //
+        mencaptran[5][5][15] = {
+                {{'t', 'o', '1'}, {'t', 'o', '2'}, {'t', 'o', '3'}, {'t', 'o', '4'}, {'t', 'o', '5'},},
+                {{'a', 'o', '2'}, {'a', 'o', '3'}, {'a', 'o', '4'}, {'a', 'o', '5'}, {'a', 'o', '6'},},
+                {{'b', 'o', '3'}, {'b', 'o', '4'}, {'t', 'o', '5'}, {'t', 'o', '6'}, {'t', 'o', '7'},},
+                {{'c', 'o', '4'}, {'c', 'o', '5'}, {'t', 'o', '6'}, {'t', 'o', '7'}, {'t', 'o', '8'},},
+                {{'d', 'o', '5'}, {'d', 'o', '6'}, {'t', 'o', '7'}, {'t', 'o', '8'}, {'t', 'o', '9'},},
+        },
+        //Abreviación del Tipo de Transistor
+        vnomtran[5][10] = {
+                {'a', 'a'},
+                {'a', 'b'},
+                {'a', 'c'},
+                {'a', 'd'},
+                {'a', 'e'},
+        },
+        //nombres de los contientes
+        vcontinom[5][10] = {
+                {'a', 's', 'i', 'a'},
+                {'a', 'm', 'e', 'r', 'i', 'c', 'a'},
+                {'a', 'f', 'r', 'i', 'c', 'a'},
+                {'o', 'c', 'e', 'a', 'n', 'i', 'a'},
+                {'e', 'u', 'r', 'o', 'p', 'a',},
+        };;
+
+
+        printf("\tAntes de iniciar el programa, captura los siguientes datos...\n\n");
+        printf("Ingresa maximo 5 continentes y su costo de envio");
+        //NOMBRE DE CONTINENTE Y DE TIPO DE TRANSISTOR
+//        for (continente = 0; continente < 5; continente++) {
+////            printf("\n\nNombre del Continente %d:", continente);
+////            scanf("%s", &vcontinom[continente - 1]);
+////            printf("\nPrecio de Envio C%d:", continente);
+////            scanf("%f", &vcostenv[continente - 1]);
+//        }
+        //NOMBRE DE ENCAP Y CANTIDAD DE ENCAP
+//        for (tipotrans = 0; tipotrans < 5; tipotrans++) {
+////            printf("\n\n\nAbreviación del Tipo de Transistor %d:", tipotrans);
+////            scanf("%s", &vnomtran[tipotrans - 1]);
+//            for (encapsulado = 0; encapsulado < 5; encapsulado++) {
+////                printf("\n\nEncapsulado del Transitor tipo %d:", encapsulado);
+////                scanf("%s", &mencaptran[tipotrans - 1][encapsulado - 1]);
+////                printf("Piezas del Encapsulado %d:", encapsulado);
+////                scanf("%d", &mcanttransis[tipotrans - 1][encapsulado - 1]);
+//
+//            }
+//        }
+        //DECLARAR ESPECIFICACIONES
+
+        printf("\n\n\n\n\n\nEn Base a la Siquiente Estructura Genera la Matriz de Especificaciones");
+        printf("\nNPN es 0 y PNP es 1");
+        printf("\n\n \t\tCosto\t\tTemp A.\t\tVoltaje\t\tCorriente\t\tNPN o PNP");
+        printf("\nUTJ 1\nUTJ 2\nUTJ 3\nUTJ 4\nUTJ 5\nBJT 6\nBJT 7\nBJT 8\nBJT 9\nBJT 10\nJFET 11\nJFET 12\nJFET 13\nJFET 14\nJFET 15\nFET 16\nFET 17\nFET 18\nFET 19\nFET 20\nFOT 21\nFOT 22\nFOT 23\nFOT 24\nFOT 25");
+//        for (espt = 1; espt <= 25; espt++) {
+//            for (esptcolum = 1; esptcolum <= 5; esptcolum++) {
+//                printf("\n\n\nFila %d / Dato %d:", espt, esptcolum);
+//                scanf("%f", &mespecificaciones[espt - 1][esptcolum - 1]);
+//            }
+//        }
+
+        // DESPLIEGUE DE VECTORES
+
+
+
+        // desplegar datos 1
+        printf("\n\n\n\t\t  Matriz de Encapsulados");
+        printf("\n\n  \tTipo de transistor \t\t\tEncapsulados\n");
+        for (tipotrans = 1; tipotrans <= 5; tipotrans++) {
+            printf("\nTransistores de Tipo %s:  ", vnomtran[tipotrans - 1]);
+
+            for (encapsulado = 1; encapsulado <= 5; encapsulado++) {
+                printf("\t %s   ", mencaptran[tipotrans - 1][encapsulado - 1]);
+            }
+        }
+
+        // desplegar datos 2
+        printf("\n\n\n\n\t\t  Matriz de Piezas Totales de un Transistor");
+        printf("\n\n  \tTipo de transistor \t\t\tCantidades\n");
+        for (tipotrans = 1; tipotrans <= 5; tipotrans++) {
+            printf("\nTransistores de Tipo %s:  ", vnomtran[tipotrans - 1]);
+
+            for (encapsulado = 1; encapsulado <= 5; encapsulado++) {
+                printf("\t %d   ", mcanttransis[tipotrans - 1][encapsulado - 1]);
+            }
+        }
+
+        //DESPLIEGUE MATRIZ ESPECIFICACIONES
+
+        printf("\n\n\n\t\t  Matriz de Especificaciones");
+        printf("\n\nEncapsulado \t\t\tEspecificaciones\n");
+        for (espt = 1; espt <= 25; espt++) {
+            printf("\nE. %d:      ", espt);
+            for (esptcolum = 1; esptcolum <= 5; esptcolum++) {
+                printf("\t %.0f   ", mespecificaciones[espt - 1][esptcolum - 1]);
+            }
+        }
+
+//CONSULTAR INCISOS
+        printf("\n\n\n\t\t     Inventario de Transistores");
+        printf("\n\n\nA. Examinar la cantidad total de piezas de un trasnsistor en especifico");
+        printf("\nB. Cambiar los datos de un componente seleccionado (siempre y cuando sea el encapsulado o las piezas totales de un transistor) ");
+        printf("\nC. Calcular promedio total de productos que se vende al año");
+        printf("\nD. Verificar la temperatura adecuada para los transistores");
+        printf("\nE. Obtener costo de envio a algun continente");
+        printf("\nF. Consulta de datasheet");
+        printf("\nG. Eliminar componentes ");
+        printf("\n\nIngresa la opción deseada :");
+        scanf("%s", &op);
+        switch (op) {
+            case 'a':
+            case 'A':
+                printf("\n\n\t\tCantidad Total de un Transistor");
+                printf("\n\nMatriticula del Transistor que quieres consultar : ");
+                scanf("%s", &buskencapsulado);
+                for (int i = 0; i < 5; ++i) {
+//                    for (int j = 0; j < ; ++j) {
+                        printf("\nTransistores de Tipo %s:  ", vnomtran[i]);
+//                    }
+                }
+
+//                for (val1 = 1; val1 <= 5; val1++) {
+//                    for (val2 = 1; val2 <= 5; val2++) {
+//                        if (mencaptran[val1 - 2][val2 - 2] == buskencapsulado)
+//                            posicion1 = val1 - 2;
+//                        posicion2 = val2 - 2;
+//                    }
+//                    printf("\n\n\tObteniendo cantidad total de piezas...\n");
+//                    printf("\n%s / %s, Catidad Total = %d ", vnomtran[posicion1], mencaptran[posicion1][posicion2],
+//                           mcanttransis[posicion1][posicion2]);
+//                }
+                break;
+
+            case 'c':
+            case 'C':
+                for (producto = 1; producto <= 25; producto = producto + 1) {
+                    printf("\n\n\n\t Los productos son: 1- T.UTJ  2- T.BJT  3- T.JFET  4- T.FET  5- T.FOT");
+                    printf("\n\n Producto %d", producto);
+                    sumprod = 0;
+                    for (mes = 1; mes <= 12; mes = mes + 1) {
+                        printf("\nProducción del mes %d :", mes);
+                        scanf("%f", &prod);
+                        sumprod = sumprod + prod;
+                    }
+                    prom = sumprod / 12;
+                    printf("\nEl promedio anual de la producción %d es %.2f", producto, prom);
+                }
+                break;
+
+        }
+//RECURSIVO
+        printf("\n\n Ingresa [1]continuar  o  [0]salir");
+        scanf("%d", &recu);
+    } while (recu == 1);
+}
