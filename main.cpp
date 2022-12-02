817bfef4fa74667219fe6a94642a86d49ed8eb66
Erick Emiliano <54825317+ErickMUOSD@users.noreply.github.com>
2022-12-02 05:40:56 -0600

    remove test array
    r

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include<locale.h>
 #include<string.h>
 
+
 int main() {
     setlocale(LC_ALL, "");
     int recu;
@@ -12,117 +13,119 @@ int main() {
     int nuevCantidad = 0;
     bool productoExiste = false, cantidadExcedida = false;
     char nuevoNombre[300];
-    int encapsulado, cantrans, tipotrans, espt, esptcolum, continente, pc, mcanttransis[5][5] = {
-            {20, 10, 50, 6666, 660},
-            {30, 30, 30, 30,   30,},
-            {40, 40, 40, 40,   40,},
-            {50, 50, 50, 50,   50,},
-            {20, 20, 20, 20,   20},
-    };
-    float mespecificaciones[25][5] = {
-            {100, 20, 3, 3, 'NPN'},
-            {100, 20, 3, 3, 'NPN'},
-            {100, 20, 3, 3, 'NPN'},
-            {100, 20, 3, 3, 'NPN'},
-            {100, 20, 3, 3, 'NPN'},
-            {100, 20, 3, 3, 'NPN'},
-            {100, 20, 3, 3, 'NPN'},
-            {100, 20, 3, 3, 'NPN'},
-            {100, 20, 3, 3, 'NPN'},
-            {100, 20, 3, 3, 'NPN'},
-            {100, 20, 3, 3, 'NPN'},
-            {100, 20, 3, 3, 'NPN'},
-            {100, 20, 3, 3, 'NPN'},
-            {100, 20, 3, 3, 'NPN'},
-            {100, 20, 3, 3, 'NPN'},
-            {100, 20, 3, 3, 'NPN'},
-            {100, 20, 3, 3, 'NPN'},
-            {100, 20, 3, 3, 'NPN'},
-            {100, 20, 3, 3, 'NPN'},
-            {100, 20, 3, 3, 'NPN'},
-            {100, 20, 3, 3, 'NPN'},
-            {100, 20, 3, 3, 'NPN'},
-            {100, 20, 3, 3, 'NPN'},
-            {100, 20, 3, 3, 'NPN'},
-            {100, 20, 3, 3, 'NPN'},
-    },
-            precioEncapsulado[5][5] = {
-            {5, 10, 20, 30, 40},
-            {5, 10, 20, 30, 40},
-            {5, 10, 20, 30, 40},
-            {5, 10, 20, 30, 40},
-            {5, 10, 20, 30, 40},
-    },
-            ventas = 0,
-            calcularVenta = 0,
+    int encapsulado, cantrans, tipotrans, espt, esptcolum, continente, pc,mcanttransis[5][5];
+//    mcanttransis[5][5] = {
+//            {20, 10, 50, 6666, 660},
+//            {30, 30, 30, 30,   30,},
+//            {40, 40, 40, 40,   40,},
+//            {50, 50, 50, 50,   50,},
+//            {20, 20, 20, 20,   20},
+//    };
+    float mespecificaciones[25][5],precioEncapsulado [5][5],ventas = 0,
+            calcularVenta = 0,  vcostenv[5] ;
+//    mespecificaciones[25][5] = {
+//            {100, 20, 3, 3, 'NPN'},
+//            {100, 20, 3, 3, 'NPN'},
+//            {100, 20, 3, 3, 'NPN'},
+//            {100, 20, 3, 3, 'NPN'},
+//            {100, 20, 3, 3, 'NPN'},
+//            {100, 20, 3, 3, 'NPN'},
+//            {100, 20, 3, 3, 'NPN'},
+//            {100, 20, 3, 3, 'NPN'},
+//            {100, 20, 3, 3, 'NPN'},
+//            {100, 20, 3, 3, 'NPN'},
+//            {100, 20, 3, 3, 'NPN'},
+//            {100, 20, 3, 3, 'NPN'},
+//            {100, 20, 3, 3, 'NPN'},
+//            {100, 20, 3, 3, 'NPN'},
+//            {100, 20, 3, 3, 'NPN'},
+//            {100, 20, 3, 3, 'NPN'},
+//            {100, 20, 3, 3, 'NPN'},
+//            {100, 20, 3, 3, 'NPN'},
+//            {100, 20, 3, 3, 'NPN'},
+//            {100, 20, 3, 3, 'NPN'},
+//            {100, 20, 3, 3, 'NPN'},
+//            {100, 20, 3, 3, 'NPN'},
+//            {100, 20, 3, 3, 'NPN'},
+//            {100, 20, 3, 3, 'NPN'},
+//            {100, 20, 3, 3, 'NPN'},
+//    },
+//            precioEncapsulado[5][5] = {
+//            {5, 10, 20, 30, 40},
+//            {5, 10, 20, 30, 40},
+//            {5, 10, 20, 30, 40},
+//            {5, 10, 20, 30, 40},
+//            {5, 10, 20, 30, 40},
+//    },
+
     //cost de envio
-    vcostenv[5] = {100.50,
-                   200.50,
-                   300.50,
-                   400.50,
-                   500.50};
-    char op, buskencapsulado[15],
+//    vcostenv[5] = {100.50,
+//                   200.50,
+//                   300.50,
+//                   400.50,
+//                   500.50};
+    char op, buskencapsulado[15],  mencaptran[5][5][15],vnomtran[5][10],  vcontinom[5][10];
     //
-    mencaptran[5][5][15] = {
-            {{'t', 'o', '1'}, {'t', 'o', '2'}, {'t', 'o', '3'}, {'t', 'o', '4'}, {'t', 'o', '5'},},
-            {{'a', 'o', '2'}, {'a', 'o', '3'}, {'a', 'o', '4'}, {'a', 'o', '5'}, {'a', 'o', '6'},},
-            {{'b', 'o', '3'}, {'b', 'o', '4'}, {'t', 'o', '5'}, {'t', 'o', '6'}, {'t', 'o', '7'},},
-            {{'c', 'o', '4'}, {'c', 'o', '5'}, {'t', 'o', '6'}, {'t', 'o', '7'}, {'t', 'o', '8'},},
-            {{'d', 'o', '5'}, {'d', 'o', '6'}, {'t', 'o', '7'}, {'t', 'o', '8'}, {'t', 'o', '9'},},
-    },
+//    mencaptran[5][5][15] = {
+//            {{'t', 'o', '1'}, {'t', 'o', '2'}, {'t', 'o', '3'}, {'t', 'o', '4'}, {'t', 'o', '5'},},
+//            {{'a', 'o', '2'}, {'a', 'o', '3'}, {'a', 'o', '4'}, {'a', 'o', '5'}, {'a', 'o', '6'},},
+//            {{'b', 'o', '3'}, {'b', 'o', '4'}, {'t', 'o', '5'}, {'t', 'o', '6'}, {'t', 'o', '7'},},
+//            {{'c', 'o', '4'}, {'c', 'o', '5'}, {'t', 'o', '6'}, {'t', 'o', '7'}, {'t', 'o', '8'},},
+//            {{'d', 'o', '5'}, {'d', 'o', '6'}, {'t', 'o', '7'}, {'t', 'o', '8'}, {'t', 'o', '9'},},
+//    },
 
     //Abreviación del Tipo de Transistor
-    vnomtran[5][10] = {
-            {'a', 'a'},
-            {'a', 'b'},
-            {'a', 'c'},
-            {'a', 'd'},
-            {'a', 'e'},
-    },
+//    vnomtran[5][10] = {
+//            {'a', 'a'},
+//            {'a', 'b'},
+//            {'a', 'c'},
+//            {'a', 'd'},
+//            {'a', 'e'},
+//    },
     //nombres de los contientes
-    vcontinom[5][10] = {
-            {'a', 's', 'i', 'a'},
-            {'a', 'm', 'e', 'r', 'i', 'c', 'a'},
-            {'a', 'f', 'r', 'i', 'c', 'a'},
-            {'o', 'c', 'e', 'a', 'n', 'i', 'a'},
-            {'e', 'u', 'r', 'o', 'p', 'a',},
-    };;
+//    vcontinom[5][10] = {
+//            {'a', 's', 'i', 'a'},
+//            {'a', 'm', 'e', 'r', 'i', 'c', 'a'},
+//            {'a', 'f', 'r', 'i', 'c', 'a'},
+//            {'o', 'c', 'e', 'a', 'n', 'i', 'a'},
+//            {'e', 'u', 'r', 'o', 'p', 'a',},
+//    };;
 
     do {
         printf("\tAntes de iniciar el programa, captura los siguientes datos...\n\n");
         printf("Ingresa maximo 5 continentes y su costo de envio");
         //NOMBRE DE CONTINENTE Y DE TIPO DE TRANSISTOR
-//        for (continente = 0; continente < 5; continente++) {
-////            printf("\n\nNombre del Continente %d:", continente);
-////            scanf("%s", &vcontinom[continente - 1]);
-////            printf("\nPrecio de Envio C%d:", continente);
-////            scanf("%f", &vcostenv[continente - 1]);
-//        }
+        for (continente = 0; continente < 5; continente++) {
+            printf("\n\nNombre del Continente %d:", continente);
+            scanf("%s", &vcontinom[continente - 1]);
+            printf("\nPrecio de Envio C%d:", continente);
+            scanf("%f", &vcostenv[continente - 1]);
+        }
         //NOMBRE DE ENCAP Y CANTIDAD DE ENCAP
-//        for (tipotrans = 0; tipotrans < 5; tipotrans++) {
-//            printf("\n\n\nAbreviación del Tipo de Transistor %d:", tipotrans);
-//            for (encapsulado = 0; encapsulado < 5; encapsulado++) {
-////                printf("\n\nEncapsulado del Transitor tipo %d:", encapsulado);
-////                scanf("%s", &vnomtran[tipotrans]);
-////                printf("Piezas del Encapsulado %d:", encapsulado);
-////                scanf("%d", &mcanttransis[tipotrans][encapsulado]);
-////
-////                printf("\\n\\Precio del encapsulado:");
-////                scanf("%f", &precioEncapsulado[tipotrans - 1][tipotrans - 1]);
-//            }
-//        }
+        for (tipotrans = 0; tipotrans < 5; tipotrans++) {
+            printf("\n\n\nAbreviación del Tipo de Transistor %d:", tipotrans);
+            for (encapsulado = 0; encapsulado < 5; encapsulado++) {
+                printf("\n\nEncapsulado del Transitor tipo %d:", encapsulado);
+                scanf("%s", &vnomtran[tipotrans]);
+                printf("Piezas del Encapsulado %d:", encapsulado);
+                scanf("%d", &mcanttransis[tipotrans][encapsulado]);
+
+                printf("\\n\\Precio del encapsulado:");
+                scanf("%f", &precioEncapsulado[tipotrans ][tipotrans ]);
+            }
+        }
         //DECLARAR ESPECIFICACIONES
 
         printf("\n\n\n\n\n\nEn Base a la Siquiente Estructura Genera la Matriz de Especificaciones");
         printf("\nNPN es 0 y PNP es 1");
         printf("\n\n \t\tCosto\t\tTemp A.\t\tVoltaje\t\tCorriente\t\tNPN o PNP");
         printf("\nUTJ 1\nUTJ 2\nUTJ 3\nUTJ 4\nUTJ 5\nBJT 6\nBJT 7\nBJT 8\nBJT 9\nBJT 10\nJFET 11\nJFET 12\nJFET 13\nJFET 14\nJFET 15\nFET 16\nFET 17\nFET 18\nFET 19\nFET 20\nFOT 21\nFOT 22\nFOT 23\nFOT 24\nFOT 25");
-//        for (espt = 1; espt <= 25; espt++) {
-//            for (esptcolum = 1; esptcolum <= 5; esptcolum++) {
-//                printf("\n\n\nFila %d / Dato %d:", espt, esptcolum);
-//                scanf("%f", &mespecificaciones[espt - 1][esptcolum - 1]);
-//            }
-//        }
+        for (espt = 1; espt <= 25; espt++) {
+            for (esptcolum = 1; esptcolum <= 5; esptcolum++) {
+                printf("\n\n\nFila %d / Dato %d:", espt, esptcolum);
+                scanf("%f", &mespecificaciones[espt - 1][esptcolum - 1]);
+            }
+        }
 
         // DESPLIEGUE DE VECTORES
 
@@ -166,7 +169,6 @@ int main() {
         printf("\n\n\nA. Examinar la cantidad total de piezas de un trasnsistor en especifico");
         printf("\nB. Cambiar los datos de un componente seleccionado (siempre y cuando sea el encapsulado o las piezas totales de un transistor) ");
         printf("\nC. Calcular promedio total de productos que se vende al año");
-        printf("\nD. Verificar la temperatura adecuada para los transistores");
         printf("\nE. Obtener costo de envio a algun continente");
         printf("\nF. Consulta de datasheet");
         printf("\nG. Eliminar componentes ");
